d88e9dc89b268990b526b60c757986c006674f2f
Ashwath V A <vaashwath@gmail.com>
2022-07-31 11:06:09 +0530

    VGA Text Mode + Tests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 // Important to disable std library
 // becoz OS cannot use anything that
 // an OS provides usually (like stdout, files, etc...)
@@ -6,31 +5,34 @@
 // To not use the normal entry point
 #![no_main]
 
-static HELLO: &[u8] = b"Hello World!";
+#![feature(custom_test_frameworks)]
+#![test_runner(MysticOS::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use MysticOS::println;
 
 // Define own start function (the entry point)
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    println!("Hello World{}", "!");
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
 
-// Have to define a handler
-// for when Rust panics
-use core::panic::PanicInfo;
-
 /// This function is called on panic
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    // Have to let it run in an infinite loop
-    // No other way
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    MysticOS::test_panic_handler(info)
 }
